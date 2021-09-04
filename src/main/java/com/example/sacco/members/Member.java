a55b1ad3516424d0f44a6a7ee2a512cd184347a6
DennisNgugi <denongugi73@gmail.com>
2021-09-04 09:50:29 +0300

    member details

--- src/main/java/com/example/sacco/members/Member.java
+++ src/main/java/com/example/sacco/members/Member.java
@@ -1,4 +1,42 @@
 package com.example.sacco.members;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Member {
+    @Id
+    @SequenceGenerator(
+            name = "member_sequence",
+            sequenceName = "member_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "member_sequence"
+    )
+    private Long memberId;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    private String phoneNumber;
+    @CreationTimestamp
+    private Date createdAt;
+    @UpdateTimestamp
+    private Date updatedAt;
+    @Embedded
+    private NextOfKin nextOfKin;
+
+
 }
---
+++ src/main/java/com/example/sacco/members/NextOfKin.java
@@ -0,0 +1,37 @@
+package com.example.sacco.members;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+@AttributeOverrides(
+        @AttributeOverride(
+                name="name",
+                column = @Column(name="next_of_kin_name")
+        ),
+        @AttributeOverride(
+                name="phoneNumber",
+                column = @Column(name="next_of_kin_mobile")
+        ),
+        @AttributeOverride(
+                name="relationship",
+                column = @Column(name="relationship")
+        ),
+)
+public class NextOfKin {
+    private String name;
+    private String phoneNumber;
+    private String relationship;
+}
